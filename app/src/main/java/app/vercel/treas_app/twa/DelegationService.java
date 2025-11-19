5595daf90f6032aa7c87ab3d52b76ab36b33f3e0
Marcos <solomonjohnmark8@gmail.com>
2025-11-19 10:34:22 +0800

    Fix: Move PWA static assets to public/ folder for Vercel deployment

---
+++ app/src/main/java/app/vercel/treas_app/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.treas_app.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
